47e7ee9ae308977b9f0b55c1b12ae041f57781e8
Zakaria <zakaria.mestour-etu@etu.univh2c.ma>
2025-04-20 19:34:56 +0100

    Connexion avec la base de données Sql et l alimention de la base de données

--- src/main/java/com/application/entities/Patient.java
+++ src/main/java/com/application/entities/Patient.java
@@ -1,13 +1,16 @@
 package com.application.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import jdk.jfr.BooleanFlag;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -15,11 +18,32 @@ import java.util.Date;
 public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+    @NotEmpty @Size(min = 2, max = 15)
     String firstName;
+    @NotEmpty @Size(min = 2, max = 15)
     String lastName;
+    @NotEmpty
+    @Pattern(regexp = "Male|Female",message =  "Gender must be either 'Male' or 'Female'")
     String gender;
+    @Pattern(regexp = "^0(6|5|7)\\d{8}$\n")
     String phoneNumber;
+    @Email
     String email;
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     Date birthDate;
+    @NotEmpty
     Boolean malade;
+    @Override
+    public String toString() {
+        return "Patient{id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender='" + gender + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", birthDate=" + birthDate +
+                ", malade=" + malade + '}';
+    }
+
 }
